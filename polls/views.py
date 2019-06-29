15e192230d5599f9e3e2ad5837d040fbc6332594
ponmathanaro <ponmathanaro@gmail.com>
2019-06-29 08:50:49 +0530

    Added index file in the template folder

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
+from django.http import HttpResponse
+from django.template import loader
 from .models import Questions
 
 # Create your views here.
 def index(request):
     # intial index func contents
     # return HttpResponse("Hello world. This is my refresher poll app")
+    # second func contents
+    # latest_question_list = Questions.objects.order_by('-pub_date')[:5]
+    # output = ','.join([q.question_text for q in latest_question_list])
+    # return  HttpResponse(output)
     latest_question_list = Questions.objects.order_by('-pub_date')[:5]
-    output = ','.join([q.question_text for q in latest_question_list])
-    return  HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list' : latest_question_list,}
+    return HttpResponse(template.render(context,request))
 def detail(request, question_id):
     return HttpResponse("you are looking at question %s." %question_id)
 def results(request, question_id):
