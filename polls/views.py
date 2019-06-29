4e61dcaa70f0bea43e66eac4ec77a5563af19d7d
ponmathanaro <ponmathanaro@gmail.com>
2019-06-29 09:57:51 +0530

    simply

--- polls/views.py
+++ polls/views.py
@@ -5,16 +5,21 @@ from .models import Questions
 
 # Create your views here.
 def index(request):
-    # intial index func contents
+    # 1st index func contents
     # return HttpResponse("Hello world. This is my refresher poll app")
-    # second func contents
+    # 2nd func contents
     # latest_question_list = Questions.objects.order_by('-pub_date')[:5]
     # output = ','.join([q.question_text for q in latest_question_list])
     # return  HttpResponse(output)
-    latest_question_list = Questions.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    # 3rd func contents
+    # latest_question_list = Questions.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {'latest_question_list' : latest_question_list,}
+    # return HttpResponse(template.render(context,request))
+    # 4th  func contents
+    latest_question_list = Questions.objects.order_by('-pub_date').[:5]
     context = {'latest_question_list' : latest_question_list,}
-    return HttpResponse(template.render(context,request))
+    return render(request,'polls/index.html',context)
 def detail(request, question_id):
     return HttpResponse("you are looking at question %s." %question_id)
 def results(request, question_id):
